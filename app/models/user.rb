4ce8b6cb144465119cc16d764e3e926651af193c
Tomasz Kula <tomaszkula07@gmail.com>
2016-04-14 16:43:16 +0200

    Add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :friendships
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => 'friend_id'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 200.times do
   User.create(
     name: Faker::Name.name,
+    password: 'test12345',
     email: Faker::Internet.email,
     birthdate: Faker::Date.between(60.years.ago, 10.years.ago),
     gender: %w{M F}.sample
