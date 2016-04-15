476589a69286f0b078d31b9a44992ac7121fb92b
Tomasz Kula <tomaszkula07@gmail.com>
2016-04-15 17:52:06 +0200

    Can like posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,15 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.create(like_params)
+    redirect_to :back
   end
 
   def destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:parent_id, :author_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.new
+    @like = Like.new
   end
 
   def new
