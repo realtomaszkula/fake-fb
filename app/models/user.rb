7eeca7ad9589ca507f67803fc69a0b9f819e259a
Tomasz Kula <tomaszkula07@gmail.com>
2016-04-15 15:38:57 +0200

    Basic post styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,9 @@ class User < ActiveRecord::Base
   # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => 'friend_id'
   # has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-
 
 
   def current_friends_ids
